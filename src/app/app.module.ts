3ae549eaaf72b9236ff7fe8a0f459ff2fef866c1
magahernandez <mghm291097@gmail.com>
2020-11-02 21:51:39 -0400

    Customize app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { Shake } from '@ionic-native/shake/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Shake,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { Shake } from '@ionic-native/shake/ngx';
+import { Platform } from '@ionic/angular'
 
 @Component({
   selector: 'app-home',
@@ -8,19 +10,36 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
   public cssClass: string;
-  private answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.',
+  public attempts: number = 0;
+  public seeAnswer: boolean = true;
+
+  /*private answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.',
              'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.',
              'Most likely.', 'Outlook good.', 'Yes.',
              'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.',
              'Better not tell you now.', 'Cannot predict now.',
              'Concentrate and ask again.', "Don't count on it.", 'My reply is no.',
-             'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+             'My sources say no.', 'Outlook not so good.', 'Very doubtful.']*/
 
-  constructor(private toastCtrl: ToastController) {}
+  private answers = [ 'No te ganaste el Fifa sorry', 'Mmmm sigues sin ganarte el Fifa', 'Felicidades, te ganaste el Fifa!' ]
+
+  constructor(private toastCtrl: ToastController, private shake: Shake, private platform: Platform) {
+
+    this.platform.ready().then(()=>{
+      this.shake.startWatch().subscribe(data => {
+        if (this.seeAnswer) this.giveAnswer()
+      })
+    })
+
+  }
+
 
   giveAnswer() {
+    this.seeAnswer = false;
     this.cssClass = "animated shake";
-    let answer = this.answers[Math.floor(Math.random() * this.answers.length)];
+    let answer = this.answers[this.attempts]
+    if (this.attempts !== 2) this.attempts += 1;
+    // let answer = this.answers[Math.floor(Math.random() * this.answers.length)];
     // this.presentToast(answer);
 
     var temp = this;
@@ -30,6 +49,7 @@ export class HomePage {
   }
 
   async presentToast(answer) {
+    this.seeAnswer = true;
     const toast = await this.toastCtrl.create({
       message: answer,
       duration: 5000,
